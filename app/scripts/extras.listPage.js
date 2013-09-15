dc5a865e68cf9ac3d7ed73c2158c26290e7a7e6b
Anas Nakawa <anas.nakawa@gmail.com>
2013-09-15 17:11:25 +0400

    relative date

--- app/scripts/extras.listPage.js
+++ app/scripts/extras.listPage.js
@@ -1,19 +1,39 @@
-'use strict';
+(function(extras) {
 
-// make sure extras object defined
-var extras = extras || {};
-extras.listPage = {};
+	'use strict';
 
-extras.listPage.init = function() {
-  
-  var self = this;
-  
-  // scaffolding classes into page
-  $('.nav-header img').each(function() {
-    var $self = $(this);
-    console.log( $.trim( $self.parent().text().toLowerCase().replace(/ /g, '-') ) );
-  });
-  
-  // this.userName = $('.nav-header img')
-  
-}
+	var issues = []
+	, pageInit = function() {
+
+		$('.jtrac-list tr').not('.sortable').each(function() {
+			var $cols = $(this).find('td')
+			, issue = {
+				  id: $cols.eq(0).text()
+				, summary: $cols.eq(1).text()
+				, status: $cols.eq(2).text()
+				, loggedBy: $cols.eq(3).text()
+				, assignedTo: $cols.eq(4).text()
+				, severity: $cols.eq(5).text()
+				, timeStamp: {
+					  element: $cols.get(6)
+					, text: $cols.eq(6).text()
+				}
+			};
+
+			issues.push( issue );
+
+			$( issue.timeStamp.element )
+				.attr( 'title', issue.timeStamp.text )
+				.text( moment( issues.timeStamp.text ).fromNow() );
+		});
+
+	}
+
+	// expose to extras API
+	// --------------------
+	extras.listPage = {
+		  init: pageInit
+		, issues: issues
+	};
+
+})( extras = extras || {}) ;
